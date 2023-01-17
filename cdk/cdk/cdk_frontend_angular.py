b07ed1da6c594a6e2684afe1f880d6863c42ccde
Luis Mori Guerra <luismorith@gmail.com>
2023-01-17 17:29:50 -0500

    base frontend angular app
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- cdk/cdk/cdk_frontend_angular.py
+++ cdk/cdk/cdk_frontend_angular.py
@@ -11,13 +11,13 @@ from aws_cdk import (
 from constructs import Construct
 
 
-class CdkStack(Stack):
+class CdkFrontendAngularStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         app_bucket = s3.Bucket(
-            self, "lguerralab-cdk-frontend-nuxt-AppBucket",
+            self, "infra-cdk-frontend-angular-app-bucket",
             website_index_document="index.html",
             website_error_document="index.html",
             public_read_access=True,
@@ -25,7 +25,7 @@ class CdkStack(Stack):
         )
 
         cf_dist = cf.CloudFrontWebDistribution(
-            self, "lguerralab-cdk-frontend-nuxt-CloudFrontDistribution",
+            self, "infra-cdk-frontend-angular-cloudfront",
             origin_configs=[
                 cf.SourceConfiguration(
                     s3_origin_source=cf.S3OriginConfig(
@@ -33,13 +33,14 @@ class CdkStack(Stack):
                     behaviors=[cf.Behavior(is_default_behavior=True)])])
 
         s3_deployment.BucketDeployment(
-            self, "lguerralab-cdk-frontend-nuxt-DeployWithInvalidation",
-            sources=[s3_deployment.Source.asset(path="../frontend-nuxt/dist")],
+            self, "infra-cdk-frontend-angular-app-bucket-deployment",
+            sources=[s3_deployment.Source.asset(
+                path="../frontend-angular/dist/frontend-angular")],
             destination_bucket=app_bucket,
             distribution=cf_dist,
             distribution_paths=["/*"]
         )
 
         CfnOutput(
-            self, "lguerralab-cdk-frontend-nuxt-CloudFrontDistributionDomainName",
+            self, "infra-cdk-frontend-angular-cloudfront-url",
             value=cf_dist.distribution_domain_name)
---
+++ frontend-angular/README.md
@@ -0,0 +1,6 @@
+### Cloud Formation 
+https://us-west-2.console.aws.amazon.com/cloudformation/home?region=us-west-2#/stacks
+
+## Stack
+
+## How to create 
